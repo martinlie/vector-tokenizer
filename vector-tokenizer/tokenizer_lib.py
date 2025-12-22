9ef0e987a6eca47c61037a8655497aa935a7e30e
Martin Lie <martin.forsberg.lie@borregaard.com>
2025-12-22 14:54:26 +0100

    Towards headless op

---
+++ vector-tokenizer/tokenizer_lib.py
@@ -0,0 +1,144 @@
+import os
+import requests
+import numpy as np
+import jax
+import jax.numpy as jnp
+import flax.linen as nn
+import optax
+from jax import value_and_grad, random, lax
+import pickle
+import pandas as pd
+import math
+from functools import partial
+from datetime import datetime
+from pathlib import Path
+
+from helper_funcs import generate, masked_fill
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+from attention_model import *
+print(jax.devices())
+
+# Disable JIT compilation
+#jax.config.update("jax_disable_jit", True)
+
+import discretize_func as discretize
+import tokenizer_func as tokenizer
+
+def get_split_data():
+      # Read
+      df = pd.read_parquet(Path("./data") / "dehli.parquet")
+
+      # Fill nans with nearest
+      df = df.ffill()
+
+      # Downsample from 5-min resolution to hourly
+      df = df.resample("h").mean()
+
+      # Downsample from 5-min resolution to quarterly
+      #df = df.resample("15min").mean()
+
+      # Change to numeric index (remove datetime)
+      #df = df.reset_index(drop=True)
+
+      # Fill nans with nearest
+      df = df.ffill()
+
+      # Columns of interest
+      cols = ['Power demand', 'temp', 'dwpt', 'rhum', 'wdir', 'wspd', 'pres']
+      df = df[cols]
+
+      # Split in train/test
+      split_idx = int(len(df) * 0.8)
+      X = df.iloc[:split_idx].copy()
+      Y = df.iloc[split_idx:].copy()
+
+      return X, Y
+
+def train(rng_key, epochs, learning_rate, train_tokens, mu, sigma, 
+            batch_size, n_channels, block_size, n_embed, num_heads, num_layers, drop_rate, 
+            vocab_size, n_bins, edges, mids):
+
+      MODEL_DIR = Path("./models")
+      MODEL_DIR.mkdir(parents=True, exist_ok=True)
+      model_name = MODEL_DIR / f"token_model_{datetime.now():%Y%m%d_%H%M%S}.pkl"
+      print("Model:", model_name)
+
+      # Set up and spin model
+      rng_key, subkey = jax.random.split(rng_key)
+      xb, yb = tokenizer.get_token_batch(train_tokens, subkey, batch_size, n_channels, block_size)
+      token_types = tokenizer.compute_token_types(xb, n_channels)
+
+      model = GPT2_v3(vocab_size, n_embed, block_size, num_heads, num_layers, drop_rate, n_channels)
+      dummy_x = jnp.zeros(shape=(batch_size, block_size), dtype=jnp.uint16)
+      dummy_token_types = jnp.zeros_like(dummy_x)
+      dummy_channel_ids = jnp.zeros_like(dummy_x)
+      variables = model.init(rng_key, dummy_x, dummy_token_types, dummy_channel_ids)
+
+      out = model.apply(variables, dummy_x, dummy_token_types, dummy_channel_ids)
+      print("Test model.apply():", out.shape)
+
+      print("Cross-entropy loss:", np.log(vocab_size))
+
+      # Establish optimizer
+      optimizer = optax.chain(
+            optax.clip_by_global_norm(1.0),
+            optax.adamw(learning_rate)
+      )
+      opt_state = optimizer.init(variables)
+      losses = []
+
+      # Training loop
+      pbar = tqdm(range(epochs))
+      for epoch in pbar:
+            rng_key, subkey = jax.random.split(rng_key)
+            xb, yb = tokenizer.get_token_batch(train_tokens, subkey, batch_size, n_channels, block_size)
+            token_types = tokenizer.compute_token_types(xb, n_channels)
+            channel_ids = tokenizer.compute_channel_ids(xb, n_channels)
+
+            loss, grads = value_and_grad(tokenizer.loss_fn, argnums=(0))(
+                  variables, 
+                  model.apply,
+                  xb, 
+                  token_types,
+                  channel_ids,
+                  n_channels,
+                  yb
+            )
+
+            if math.isnan(loss):
+                  print(f"Premature stop (loss=nan) at epoch {epoch}")
+                  break
+
+            updates, opt_state = optimizer.update(grads, opt_state, variables)
+            variables = optax.apply_updates(variables, updates)
+            losses.append(loss)
+            
+            pbar.set_description(f"Epoch: {epoch}, Loss: {loss :.4f}")
+
+            model_file = {
+                  "epochs": epochs,
+                  "epoch": epoch,
+                  "model": model,
+                  "vocab_size": vocab_size,
+                  "block_size": block_size,
+                  "variables": variables,
+                  "losses": losses,
+                  "opt_state": opt_state,
+                  "learning_rate": learning_rate,
+                  "n_channels": n_channels,
+                  "n_bins": n_bins,
+                  "edges": edges, 
+                  "mids": mids,
+                  "mu": mu,
+                  "sigma": sigma,
+            }
+
+            # Save model every 10th epoch or if last
+            is_every_10 = (epoch + 1) % 10 == 0
+            is_last = (epoch == epochs - 1)
+            if is_every_10 or is_last:
+                  with open(model_name, 'wb') as f:
+                        pickle.dump(model_file, f)
+
+      return model_file
---
+++ vector-tokenizer/train.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import numpy as np
+import jax
+import jax.numpy as jnp
+import tokenizer_lib as tl
+import discretize_func as discretize
+import tokenizer_func as tokenizer
+
+# Parameters
+n_bins=1024 # discretization fit bins
+n_embed = 16 # 32 # Number of embedding dimensions
+batch_size = 8 #4 # How many independent sequences will we process in parallel?
+block_size = 480 # What is the maximum context length for predictions?
+num_heads = 4 # Number of heads in the multi-headed block
+num_layers = 6 # Number of transformer decoder blocks
+drop_rate = 0.1 # Dropout rate for regularization
+learning_rate = 1e-5
+epochs = 15
+
+rng_key = jax.random.PRNGKey(42)
+
+if __name__ == "__main__":
+      
+      # Load splitted data
+      X, Y = tl.get_split_data()
+      print(f"X: {X.shape}")
+      print(f"Y: {Y.shape}")
+
+      # Z-norm standardisation
+      mu = X.mean(axis=0)
+      sigma = X.std(axis=0) + 1e-8
+      X_normalized = (X-mu)/sigma
+
+      # Derivation
+      X_normalized = X_normalized.diff().dropna()
+      X_nv = X_normalized.values.astype(np.float32)   # (N, D)
+
+      # Discretization
+      #edges, mids = discretize.fit_quantile_bins_global(X_nv, n_bins=n_bins)
+      edges, mids = discretize.fit_equal_width_bins_global(X_nv, n_bins=n_bins)
+
+      # encode
+      #X_tok = discretize.encode_quantile_global(jnp.asarray(X_nv), edges)
+      X_tok = discretize.encode_equal_width_global(jnp.asarray(X_nv), edges)
+
+      # decode
+      #X_rec = discretize.decode_quantile_global(X_tok, mids)
+      X_rec = discretize.decode_equal_width_global(X_tok, mids)
+
+      ZERO_BIN = int(np.argmin(np.abs(mids)))
+      #or
+      assert(ZERO_BIN == discretize.encode_equal_width_global(jnp.asarray([0]), edges))
+
+      print("Zero midpoint:", mids[ZERO_BIN])
+      print("Index:", ZERO_BIN)
+
+      # Tokenize
+      n_channels = len(X.columns)
+      #tokens = tokenizer.encode_with_channels(X_tok, n_channels)
+      tokens = tokenizer.encode_with_channels_sparse(X_tok, n_channels, ZERO_BIN)
+
+      DATA_OFFSET = 2 + n_channels  # BOS+EOS+n_channels = 9
+      vocab_size = DATA_OFFSET + n_bins
+
+      T = tokens.shape[0]
+      print("Total tokens:", T)
+      print("Vocab size:", vocab_size)
+
+      # Inspect first few tokens
+      print("Last 100 tokens:", tokens[:100])
+
+      # Train
+      model_file = tl.train(rng_key, epochs, learning_rate, 
+            tokens, mu, sigma, 
+            batch_size, n_channels, block_size, n_embed, num_heads, num_layers, drop_rate, 
+            vocab_size, n_bins, edges, mids)
+
